d692e8034405708adb89ac1ceb92913f94c6fa3a
ElanaSimon95 <ElanaSimon95@gmail.com>
2016-12-06 03:00:23 -0500

    adding simple sharding test. should be deleted later once sharding is integrated

---
+++ poker/holdem/sharding_test.py
@@ -0,0 +1,20 @@
+import argparse
+import time
+from deuces import Card, Deck, Evaluator
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', help='iteration')
+parser.add_argument('-n', help='number_hash_bins')
+
+i = int(parser.parse_args().i)
+N = int(parser.parse_args().n)
+
+start_time = time.time()
+for _ in range(100):
+    board = None
+    while board == None:
+        deck = Deck()
+        b = deck.draw(5)
+        if hash(str(b)) % N == i:
+            board = b
+print ("Average time to find a board hash match is %f seconds using %s bins" %((time.time() - start_time)/100., N))
